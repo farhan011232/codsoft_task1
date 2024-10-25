25eeb07724af2b29f4ef89741a358de31c5b7204
farhan011232 <113981104+farhan011232@users.noreply.github.com>
2024-10-25 13:57:06 +0530

    Update todo_list.py

--- todo_list.py
+++ todo_list.py
@@ -1,8 +1,8 @@
-# importing the required modules  
-import tkinter as tk                    # importing the tkinter module as tk  
-from tkinter import ttk                 # importing the ttk module from the tkinter library  
-from tkinter import messagebox          # importing the messagebox module from the tkinter library  
-import sqlite3 as sql                   # importing the sqlite3 module as sql  
+
+import tkinter as tk                  
+from tkinter import ttk              
+from tkinter import messagebox         
+import sqlite3 as sql                   
   
 # defining the function to add tasks to the list  
 def add_task():  
